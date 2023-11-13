ab62f219c146cf9c9cc60c72e9fe182ef84b0424
Abhi-Survase <abhishek.survase2000@gmail.com>
2023-11-14 01:35:40 +0530

    added more truth and dare statements

--- lib/truthordare_data.dart
+++ lib/truthordare_data.dart
@@ -40,7 +40,7 @@ const todData = [
   ),
   TodModel(
     'What’s your biggest regret?',
-    'Say two honest things about everyone else in the group',
+    'Say two honest things about someone you hate',
   ),
   TodModel(
     'If you had to only ever watch rom-coms or only watch scary movies for the rest of your life, which would you choose and why?',
@@ -64,7 +64,7 @@ const todData = [
   ),
   TodModel(
     'What excuse have you used before to get out plans?',
-    'Show your orgasm face',
+    'Smell another player\'s armpit',
   ),
   TodModel(
     'What\'s the longest you\'ve ever slept?',
@@ -81,5 +81,109 @@ const todData = [
   TodModel(
     'What\'s your biggest pet peeve?',
     'Show the last five people you texted and what the messages said',
+  ),
+  TodModel(
+    'Even if you’d be paid \$1 million for it, what\'s something you would never do?',
+    'What is the most daring thing you\'ve ever done on a dare?',
+  ),
+  TodModel(
+    'Have you ever said “I love you” to someone and not meant it?',
+    'Draw your favorite movie and have the other person guess it (Pictionary-style).',
+  ),
+  TodModel(
+    "What’s the shortest you’ve ever slept?",
+    "Lick a bar of soap.",
+  ),
+  TodModel(
+    "What's the most embarrassing thing you ever did on a date?",
+    "Talk to a pillow as if it’s your crush.",
+  ),
+  TodModel(
+    'What is the boldest pickup line you\'ve ever used?',
+    'Find your very first/previous crush on social and DM them.',
+  ),
+  TodModel(
+    'How many selfies do you take a day?',
+    'Let the group mix together five of whatever liquids they find in the fridge, then drink it.',
+  ),
+  TodModel(
+    'What\'s the most embarrassing top-played song on your phone?',
+    'Allow the person to your right to draw on your face with a Sharpie.',
+  ),
+  TodModel(
+    'If you could be a fictional character for a day, who would you choose?',
+    'Post a flirty comment on the first Instagram picture that you see.',
+  ),
+  TodModel(
+    'If you could date a fictional character, who would it be?',
+    'Pole dance with an imaginary pole',
+  ),
+  TodModel(
+    'What\'s one silly thing you can\'t live without?',
+    'Post the oldest selfie on your phone on Instagram Stories',
+  ),
+  TodModel(
+    'What is the weirdest trend you\'ve ever participated in?',
+    'Pretend to be the person to your right for 5 minutes',
+  ),
+  TodModel(
+    'Who do you text the most?',
+    'Say something dirty to the person on your left',
+  ),
+  TodModel(
+    'What\'s an instant deal breaker in a potential love interest?',
+    'Do a cartwheel.',
+  ),
+  TodModel(
+    'What is the biggest lie you ever told your parents?',
+    'Let another player post a status on your social.',
+  ),
+  TodModel(
+    'Have you ever fallen in love at first sight?',
+    'Crack an egg on your head.',
+  ),
+  TodModel(
+    'What\'s the weirdest thing you\'ve ever eaten?',
+    'Call a friend, pretend it\'s their birthday.',
+  ),
+  TodModel(
+    'Tell us about the biggest romantic fail you\'ve ever experienced.',
+    'Take a selfie on the toilet and post it.',
+  ),
+  TodModel(
+    'Who was your first celebrity crush?',
+    'Let someone paint your nails any way they want.',
+  ),
+  TodModel(
+    'Name one thing you\'d do if you knew there\'d be zero consequences.',
+    'Let everyone give you a romantic nickname.',
+  ),
+  TodModel(
+    'Is there anyone you stalk on social media?',
+    'Call the last person you texted and just “meow” into the phone.',
+  ),
+  TodModel(
+    'Do you still have feelings for any of your exes?',
+    'Go on social and like every photo of the first person you see.',
+  ),
+  TodModel(
+    'What\'s the most embarrassing thing you\'ve done to get a crush\'s attention?',
+    'Take an item out of the refrigerator and passionately kiss it.',
+  ),
+  TodModel(
+    'What are the five most recent things in your search history?',
+    'Have you ever had a crush on a teacher or professor?',
+  ),
+  TodModel(
+    'Who was the first person you said “I love you” to?',
+    'Reveal all the details of your first kiss.',
+  ),
+  TodModel(
+    'What\'s the most awkward thing you\'ve ever been caught doing?',
+    'Let the other players redo your hairstyle.',
+  ),
+  TodModel(
+    'If you had to get a tattoo today, what would it be?',
+    'Take a shot of pickle juice.',
   ),
 ];
